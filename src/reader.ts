ba5701c8371f2e2b6b79464c9a34738346ec5893
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-16 23:28:16 -0400

    Implement Chrome's text to speech engine

--- src/reader.ts
+++ src/reader.ts
@@ -1,5 +1,27 @@
-function read(selection: string): void {
-  console.log(selection);
+const OPTIONS: chrome.tts.SpeakOptions = {
+  lang: "en-GB",
+  rate: 1.5,
+  enqueue: true,
+  pitch: 0,
+  volume: 1,
+  onEvent: logSpeechEvents
+};
+
+function read(
+  utterances: string,
+  options: chrome.ttsEngine.SpeakOptions = OPTIONS
+): void {
+  chrome.tts.speak(utterances, options, logSpeakError);
+}
+
+function logSpeakError(): void {
+  if (chrome.runtime.lastError)
+    console.log(`Error: ${chrome.runtime.lastError.message}`);
+}
+
+function logSpeechEvents(event: chrome.tts.TtsEvent): void {
+  console.log(`Event ${event.type} at position ${event.charIndex})`);
+  if (event.type === "error") console.log(`Error: ${event.errorMessage}`);
 }
 
 export { read as default };
