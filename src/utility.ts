4be09df288f02f4f0e91411a35a365135ad7750c
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-18 18:27:40 -0400

    Inline get tab function
    Signed-off-by: Lateef Alabi-Oki <mystilleef@gmail.com>

--- src/utility.ts
+++ src/utility.ts
@@ -1,18 +1,12 @@
 function queryContentForSelection(): void {
   chrome.tabs.query(
     { active: true, currentWindow: true },
-    sendSelectionQueryMessage
+    (tabs: Array<chrome.tabs.Tab>): boolean => {
+      const tabid = tabs[0].id || -1;
+      if (tabid) chrome.tabs.sendMessage(tabid, { query: "GET_SELECTION" });
+      return true;
+    }
   );
-}
-
-function sendSelectionQueryMessage(tabs: Array<chrome.tabs.Tab>): boolean {
-  const tabid = getTabId(tabs);
-  if (tabid) chrome.tabs.sendMessage(tabid, { query: "GET_SELECTION" });
-  return true;
-}
-
-function getTabId(tabs: Array<chrome.tabs.Tab>): number {
-  return tabs[0].id || -1;
 }
 
 export { queryContentForSelection as default };
