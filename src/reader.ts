62f6bae623de284791edc5278a98c61e9765e4dc
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-18 15:47:32 -0400

    Remove empty phrases from utterances
    Signed-off-by: Lateef Alabi-Oki <mystilleef@gmail.com>

--- src/reader.ts
+++ src/reader.ts
@@ -1,7 +1,4 @@
-import updateBrowserIcon from "./icon";
-
-// const BY_COMMON_PUNCTUATIONS = /[|(){}<>[\]_/—*.,~:;=!?]/gm;
-const BY_COMMON_PUNCTUATIONS = /[_.,:;!?]/gm;
+const BY_COMMON_PUNCTUATIONS = /[_.,:;!?<>/]/gm;
 
 const OPTIONS: chrome.tts.SpeakOptions = {
   pitch  : 0,
@@ -10,9 +7,7 @@ const OPTIONS: chrome.tts.SpeakOptions = {
   lang   : "en-GB",
   enqueue: true,
   onEvent: (event: chrome.tts.TtsEvent): void => {
-    chrome
-      .tts
-      .isSpeaking((speaking: boolean) => updateBrowserIcon(speaking));
+    chrome.tts.isSpeaking((speaking: boolean) => updateBrowserIcon(speaking));
     if (event.type === "error") console.log(`Error: ${event.errorMessage}`);
   }
 };
@@ -21,10 +16,15 @@ function read(
   utterances: string,
   options: chrome.ttsEngine.SpeakOptions = OPTIONS
 ): void {
-  const phrases = utterances.split(BY_COMMON_PUNCTUATIONS);
-  phrases.forEach(
-    phrase => chrome.tts.speak(phrase.trim(), options, logSpeakEventErrors)
-  );
+  utterances
+    .split(BY_COMMON_PUNCTUATIONS)
+    .map(phrase => phrase.trim())
+    .filter(phrase => phrase.length)
+    .forEach(phrase => speak(phrase, options));
+}
+
+function speak(phrase: string, options: chrome.ttsEngine.SpeakOptions): void {
+  chrome.tts.speak(phrase, options, logSpeakEventErrors);
 }
 
 function logSpeakEventErrors(): void {
