f3c9e5c4dad72c8dcbf7e93b373f4b75b4a3bca0
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-22 20:46:10 -0400

    feat: Log potential errors when using Chrome storage APIs
    Signed-off-by: Lateef Alabi-Oki <mystilleef@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,20 @@
+import { logChromeErrorMessage } from "./error";
+
 const RATE              = "rate";
 const VOICENAME         = "voiceName";
 const PITCH             = "pitch";
 const DEFAULT_VOICENAME = "Google UK English Female";
 
 const setPitch = (pitch: number): void => {
-  chrome.storage.sync.set({ pitch }, () => {});
+  chrome.storage.sync.set({ pitch }, () => logChromeErrorMessage());
 };
 
 const setRate = (rate: number): void => {
-  chrome.storage.sync.set({ rate }, () => {});
+  chrome.storage.sync.set({ rate }, () => logChromeErrorMessage());
 };
 
 const setVoiceName = (voiceName: string): void => {
-  chrome.storage.sync.set({ voiceName }, () => {});
+  chrome.storage.sync.set({ voiceName }, () => logChromeErrorMessage());
 };
 
 export {
