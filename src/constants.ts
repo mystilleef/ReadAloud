f8f063bccdca10e5d93ccdd6abad8491a33635ec
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-25 17:23:24 -0400

    refactor: Add stronger typing for interface
    Signed-off-by: Lateef Alabi-Oki <mystilleef@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -4,28 +4,20 @@ const PITCH             = "pitch";
 const VOICENAME         = "voiceName";
 const DEFAULT_RATE      = 1.2;
 const DEFAULT_PITCH     = 0;
-const DEFAULT_VOLUME    = 1;
 const DEFAULT_VOICENAME = "Google UK English Female";
 
-const DEFAULT_OPTIONS = {
-  pitch    : DEFAULT_PITCH,
-  volume   : DEFAULT_VOLUME,
-  rate     : DEFAULT_RATE,
-  voiceName: DEFAULT_VOICENAME,
-  enqueue  : true
-};
-
 interface VoiceStorageOptions {
-  rate?: number;
-  pitch?: number;
-  voiceName?: string;
+  readonly [index: string]: number | string | undefined;
+
+  readonly rate?: number;
+  readonly pitch?: number;
+  readonly voiceName?: string;
 }
 
 export {
   RATE,
   PITCH,
   VOICENAME,
-  DEFAULT_OPTIONS,
   DEFAULT_VOICENAME,
   DEFAULT_RATE,
   DEFAULT_PITCH,
