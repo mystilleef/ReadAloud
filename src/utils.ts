7706caf81b2af02d466e1579d8d08d1d804a7a9d
Lateef Alabi-Oki <mystilleef@gmail.com>
2019-08-29 02:43:59 -0400

    style: Format for clarity
    Signed-off-by: Lateef Alabi-Oki <mystilleef@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,11 @@
 const getTtsVoices = async (): Promise<chrome.tts.TtsVoice[]> =>
-  new Promise((resolve): void => {
-    chrome.tts.getVoices(resolve);
-  });
+  new Promise((resolve): void => chrome.tts.getVoices(resolve));
 
 const isSpeaking = async (): Promise<boolean> =>
-  new Promise((
-    (resolve): void => {
-      chrome.tts.isSpeaking(resolve);
-    }
-  ));
+  new Promise(
+    (
+      (resolve): void => chrome.tts.isSpeaking(resolve)
+    )
+  );
 
 export { getTtsVoices, isSpeaking };
