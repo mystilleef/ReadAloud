9cadb4089bea06e058c5ad33c20925614640742b
Lateef Alabi-Oki <mystilleef@gmail.com>
2021-05-04 12:29:28 -0400

    fix: tts hang bug

--- src/ttshandler.ts
+++ src/ttshandler.ts
@@ -3,6 +3,8 @@ import { logError } from "./error";
 import updateBrowserIcon from "./icon";
 import { isSpeaking } from "./utils";
 
+let TIMEOUT_RESUME_SPEAKING = 0;
+
 export function onTtsEvent(event: chrome.tts.TtsEvent): void {
   isSpeaking().then(updateBrowserIcon).catch(logError);
   handleTtsEvent(event);
@@ -10,12 +12,32 @@ export function onTtsEvent(event: chrome.tts.TtsEvent): void {
 
 function handleTtsEvent(event: chrome.tts.TtsEvent): void {
   if (event.type === "error") handleError(`Error: ${event.errorMessage}`);
-  else if (event.type === "end") badgeCounter.decrement();
+  else if (event.type === "start") handleStart();
+  else if (event.type === "end") handleEnd();
 }
 
 function handleError(message: string): void {
   stop();
   logError(message);
+}
+
+function handleStart() {
+  resumeSpeaking();
+}
+
+function handleEnd() {
+  stopSpeaking();
+  badgeCounter.decrement();
+}
+
+function resumeSpeaking() {
+  chrome.tts.pause();
+  chrome.tts.resume();
+  TIMEOUT_RESUME_SPEAKING = window.setTimeout(resumeSpeaking, 10000);
+}
+
+function stopSpeaking() {
+  clearTimeout(TIMEOUT_RESUME_SPEAKING);
 }
 
 export function stop(): void {
