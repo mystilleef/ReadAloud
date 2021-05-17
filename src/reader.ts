fa6a5fac685802587d54a1eeff05ab467a2c8d4c
Lateef Alabi-Oki <mystilleef@gmail.com>
2021-05-17 10:34:50 -0400

    feat: make speak api asynchronous

--- src/reader.ts
+++ src/reader.ts
@@ -4,25 +4,29 @@ import { getStorageOptions } from "./storage";
 import { onTtsEvent, stop } from "./ttshandler";
 
 async function read(utterances: string): Promise<void> {
-  const speakOptions = await getSpeakOptions();
-  speak(utterances, speakOptions);
+  await speak(utterances, await getSpeakOptions());
+}
+
+async function speak(
+  phrase: string,
+  options: chrome.ttsEngine.SpeakOptions
+): Promise<void> {
+  return new Promise<void>((_resolve, _reject) => {
+    chrome.tts.speak(phrase, options, logChromeErrorMessage);
+    badgeCounter.increment();
+  });
 }
 
 async function getSpeakOptions(): Promise<chrome.tts.SpeakOptions> {
   const { rate, pitch, voiceName } = await getStorageOptions();
   return {
-    pitch    : pitch as number,
-    rate     : rate as number,
+    pitch: pitch as number,
+    rate: rate as number,
     voiceName: voiceName as string,
-    volume   : 1,
-    enqueue  : true,
-    onEvent  : onTtsEvent
+    volume: 1,
+    enqueue: true,
+    onEvent: onTtsEvent
   };
-}
-
-function speak(phrase: string, options: chrome.ttsEngine.SpeakOptions): void {
-  chrome.tts.speak(phrase, options, logChromeErrorMessage);
-  badgeCounter.increment();
 }
 
 export { read, stop };
