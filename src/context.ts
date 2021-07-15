88002a5df6f800ebae6c1237ff6dd486cddb774d
Lateef Alabi-Oki <mystilleef@gmail.com>
2021-07-15 14:09:39 -0400

    style: reformat code

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,4 @@
-import { EXTENSION_ID } from "./constants";
+import {EXTENSION_ID} from "./constants";
 import {
   chromeRuntimeError,
   doNothing,
@@ -15,46 +15,46 @@ import {
   storeVoice,
   VoiceStorageOptions
 } from "./storage";
-import { getTtsVoices } from "./utils";
+import {getTtsVoices} from "./utils";
 
 const SUBMENU_ID_DELIMETER = "|";
 
 const keyFromId = (id: string): string => `${id}${SUBMENU_ID_DELIMETER}`;
 
-const UNIQUE_STAMP            = `${EXTENSION_ID}`;
-const SPEED_MENU_ID           = `ReadAloudSpeedMenu-${UNIQUE_STAMP}`;
-const SPEED_SUBMENU_ID_KEY    = keyFromId(SPEED_MENU_ID);
-const PITCH_MENU_ID           = `ReadAloudPitchMenu-${UNIQUE_STAMP}`;
-const PITCH_SUBMENU_ID_KEY    = keyFromId(PITCH_MENU_ID);
-const VOICES_MENU_ID          = `ReadAloudVoicesMenu-${UNIQUE_STAMP}`;
-const VOICES_SUBMENU_ID_KEY   = keyFromId(VOICES_MENU_ID);
+const UNIQUE_STAMP = `${EXTENSION_ID}`;
+const SPEED_MENU_ID = `ReadAloudSpeedMenu-${UNIQUE_STAMP}`;
+const SPEED_SUBMENU_ID_KEY = keyFromId(SPEED_MENU_ID);
+const PITCH_MENU_ID = `ReadAloudPitchMenu-${UNIQUE_STAMP}`;
+const PITCH_SUBMENU_ID_KEY = keyFromId(PITCH_MENU_ID);
+const VOICES_MENU_ID = `ReadAloudVoicesMenu-${UNIQUE_STAMP}`;
+const VOICES_SUBMENU_ID_KEY = keyFromId(VOICES_MENU_ID);
 const READ_ALOUD_ROOT_MENU_ID = `ReadAloudMenu-${UNIQUE_STAMP}`;
-const RESET_DEFAULT_MENU_ID   = `ReadAloudResetDefaultMenu-${UNIQUE_STAMP}`;
+const RESET_DEFAULT_MENU_ID = `ReadAloudResetDefaultMenu-${UNIQUE_STAMP}`;
 
-const CONTEXTS            = ["all"];
+const CONTEXTS = ["all"];
 const TOP_LEVEL_MENU_INFO = [
   {
-    title   : "Speed",
+    title: "Speed",
     parentId: READ_ALOUD_ROOT_MENU_ID,
-    id      : SPEED_MENU_ID,
+    id: SPEED_MENU_ID,
     contexts: CONTEXTS
   },
   {
-    title   : "Voices",
+    title: "Voices",
     parentId: READ_ALOUD_ROOT_MENU_ID,
-    id      : VOICES_MENU_ID,
+    id: VOICES_MENU_ID,
     contexts: CONTEXTS
   },
   {
-    title   : "Pitch",
+    title: "Pitch",
     parentId: READ_ALOUD_ROOT_MENU_ID,
-    id      : PITCH_MENU_ID,
+    id: PITCH_MENU_ID,
     contexts: CONTEXTS
   },
   {
-    title   : "Reset to Default",
+    title: "Reset to Default",
     parentId: READ_ALOUD_ROOT_MENU_ID,
-    id      : RESET_DEFAULT_MENU_ID,
+    id: RESET_DEFAULT_MENU_ID,
     contexts: CONTEXTS
   }
 ];
@@ -65,8 +65,8 @@ const PITCH_OPTIONS = [0, 0.5, 1, 1.5, 2];
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create(
     {
-      title   : "Read Aloud",
-      id      : READ_ALOUD_ROOT_MENU_ID,
+      title: "Read Aloud",
+      id: READ_ALOUD_ROOT_MENU_ID,
       contexts: CONTEXTS
     },
     () => {
@@ -76,21 +76,24 @@ chrome.runtime.onInstalled.addListener(() => {
   );
 });
 
-function createTopLevelMenus(
-  menu: { id: string; title: string; parentId: string; contexts: string[] }
-): void {
-  chrome.contextMenus.create(
-    menu,
-    () => {
-      if (chromeRuntimeError()) logChromeErrorMessage();
-      else createRadioMenuItems(menu);
-    }
-  );
+function createTopLevelMenus(menu: {
+  id: string;
+  title: string;
+  parentId: string;
+  contexts: string[];
+}): void {
+  chrome.contextMenus.create(menu, () => {
+    if (chromeRuntimeError()) logChromeErrorMessage();
+    else createRadioMenuItems(menu);
+  });
 }
 
-function createRadioMenuItems(
-  menu: { id: string; title: string; parentId: string; contexts: string[] }
-): void {
+function createRadioMenuItems(menu: {
+  id: string;
+  title: string;
+  parentId: string;
+  contexts: string[];
+}): void {
   switch (menu.id) {
     case SPEED_MENU_ID:
       createSpeedRadioMenuItems(menu).catch(logError);
@@ -105,56 +108,71 @@ function createRadioMenuItems(
   }
 }
 
-async function createSpeedRadioMenuItems(
-  menu: { id: string; title: string; parentId: string; contexts: string[] }
-): Promise<void> {
+async function createSpeedRadioMenuItems(menu: {
+  id: string;
+  title: string;
+  parentId: string;
+  contexts: string[];
+}): Promise<void> {
   const rate = await getRate();
-  SPEED_OPTIONS.forEach(speed => chrome.contextMenus.create(
-    {
-      parentId: menu.id,
-      contexts: menu.contexts,
-      type    : "radio",
-      title   : `${speed}x`,
-      id      : `${SPEED_SUBMENU_ID_KEY}${speed}`,
-      checked : rate === Number(speed)
-    },
-    logChromeErrorMessage
-  ));
+  SPEED_OPTIONS.forEach(speed =>
+    chrome.contextMenus.create(
+      {
+        parentId: menu.id,
+        contexts: menu.contexts,
+        type: "radio",
+        title: `${speed}x`,
+        id: `${SPEED_SUBMENU_ID_KEY}${speed}`,
+        checked: rate === Number(speed)
+      },
+      logChromeErrorMessage
+    )
+  );
 }
 
-async function createVoicesRadioMenuItems(
-  menu: { id: string; title: string; parentId: string; contexts: string[] }
-): Promise<void> {
-  const voices    = await getTtsVoices();
+async function createVoicesRadioMenuItems(menu: {
+  id: string;
+  title: string;
+  parentId: string;
+  contexts: string[];
+}): Promise<void> {
+  const voices = await getTtsVoices();
   const voiceName = await getVoiceName();
-  voices.forEach(voice => chrome.contextMenus.create(
-    {
-      parentId: menu.id,
-      contexts: menu.contexts,
-      type    : "radio",
-      title   : `${voice.voiceName}`,
-      id      : `${VOICES_SUBMENU_ID_KEY}${voice.voiceName}`,
-      checked : voiceName === voice.voiceName
-    },
-    logChromeErrorMessage
-  ));
+  voices.forEach(voice =>
+    chrome.contextMenus.create(
+      {
+        parentId: menu.id,
+        contexts: menu.contexts,
+        type: "radio",
+        title: `${voice.voiceName}`,
+        id: `${VOICES_SUBMENU_ID_KEY}${voice.voiceName}`,
+        checked: voiceName === voice.voiceName
+      },
+      logChromeErrorMessage
+    )
+  );
 }
 
-async function createPitchRadioMenuItems(
-  menu: { id: string; title: string; parentId: string; contexts: string[] }
-): Promise<void> {
+async function createPitchRadioMenuItems(menu: {
+  id: string;
+  title: string;
+  parentId: string;
+  contexts: string[];
+}): Promise<void> {
   const pitchFromStore = await getPitch();
-  PITCH_OPTIONS.forEach(pitch => chrome.contextMenus.create(
-    {
-      parentId: menu.id,
-      type    : "radio",
-      contexts: menu.contexts,
-      title   : `${pitch}`,
-      id      : `${PITCH_SUBMENU_ID_KEY}${pitch}`,
-      checked : pitchFromStore === Number(pitch)
-    },
-    logChromeErrorMessage
-  ));
+  PITCH_OPTIONS.forEach(pitch =>
+    chrome.contextMenus.create(
+      {
+        parentId: menu.id,
+        type: "radio",
+        contexts: menu.contexts,
+        title: `${pitch}`,
+        id: `${PITCH_SUBMENU_ID_KEY}${pitch}`,
+        checked: pitchFromStore === Number(pitch)
+      },
+      logChromeErrorMessage
+    )
+  );
 }
 
 chrome.contextMenus.onClicked.addListener((info, _tab) => {
@@ -166,16 +184,13 @@ chrome.contextMenus.onClicked.addListener((info, _tab) => {
 function onRadioMenuItemClick(info: chrome.contextMenus.OnClickData): void {
   switch (info.parentMenuItemId) {
     case VOICES_MENU_ID:
-      storeVoice(stringValueFrom(info.menuItemId))
-        .catch(logError);
+      storeVoice(stringValueFrom(info.menuItemId)).catch(logError);
       break;
     case SPEED_MENU_ID:
-      storeRate(numberValueFrom(info.menuItemId))
-        .catch(logError);
+      storeRate(numberValueFrom(info.menuItemId)).catch(logError);
       break;
     case PITCH_MENU_ID:
-      storePitch(numberValueFrom(info.menuItemId))
-        .catch(logError);
+      storePitch(numberValueFrom(info.menuItemId)).catch(logError);
       break;
     default:
       logError("ERROR: Invalid menu item parameter");
@@ -190,15 +205,13 @@ function numberValueFrom(id: string): number {
   return Number(id.split(SUBMENU_ID_DELIMETER).pop());
 }
 
-chrome.storage.onChanged.addListener((
-  _changes: chrome.storage.StorageChange,
-  _areaName: string
-) => resolveStorageConfigurations());
+chrome.storage.onChanged.addListener(
+  (_changes: chrome.storage.StorageChange, _areaName: string) =>
+    resolveStorageConfigurations()
+);
 
 function resolveStorageConfigurations(): void {
-  getStorageOptions()
-    .then(updateSubMenus)
-    .catch(doNothing);
+  getStorageOptions().then(updateSubMenus).catch(doNothing);
 }
 
 function updateSubMenus(result: VoiceStorageOptions): void {
@@ -207,11 +220,9 @@ function updateSubMenus(result: VoiceStorageOptions): void {
     `${VOICES_SUBMENU_ID_KEY}${result.voiceName}`,
     `${SPEED_SUBMENU_ID_KEY}${result.rate}`
   ];
-  MENU_IDS.forEach(id => chrome.contextMenus.update(
-    id,
-    { checked: true },
-    logChromeErrorMessage
-  ));
+  MENU_IDS.forEach(id =>
+    chrome.contextMenus.update(id, {checked: true}, logChromeErrorMessage)
+  );
 }
 
 function resetToDefault(): void {
