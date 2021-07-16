852392444aeddb06112f93a4a982d162693c96f4
Lateef Alabi-Oki <mystilleef@gmail.com>
2021-07-16 08:58:17 -0400

    chore: update dependencies

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
-import { logError } from "./error";
-import { read, stop } from "./reader";
-import { isSpeaking } from "./utils";
+import {logError} from "./error";
+import {read, stop} from "./reader";
+import {isSpeaking} from "./utils";
 
 const COMMAND = "read-aloud-selected-text";
 
@@ -13,22 +13,18 @@ function handleChromeCommand(command: string): void {
 chrome.browserAction.onClicked.addListener(handleBrowserAction);
 
 function handleBrowserAction(_tab: chrome.tabs.Tab): void {
-  const stopOrQuery = (speaking: boolean): void => speaking
-    ? stop()
-    : queryContentForSelection();
+  const stopOrQuery = (speaking: boolean): void =>
+    speaking ? stop() : queryContentForSelection();
   isSpeaking().then(stopOrQuery).catch(logError);
 }
 
 function queryContentForSelection(): void {
   chrome.tabs.query(
-    { active: true, currentWindow: true },
+    {active: true, currentWindow: true},
     (tabs: chrome.tabs.Tab[]): void => {
       // eslint-disable-next-line no-magic-numbers
       const tabid = tabs[0].id || -1;
-      if (tabid) chrome.tabs.sendMessage(
-        tabid,
-        { query: "GET_SELECTION" }
-      );
+      if (tabid) chrome.tabs.sendMessage(tabid, {query: "GET_SELECTION"});
     }
   );
 }
@@ -36,9 +32,9 @@ function queryContentForSelection(): void {
 chrome.runtime.onMessage.addListener(handleReadSelectionMessage);
 
 function handleReadSelectionMessage(
-  request: { message: string; selection: string; speaking: boolean; },
+  request: {message: string; selection: string; speaking: boolean},
   sender: chrome.runtime.MessageSender,
-  _senderResponse: (response: { result: string; }) => void
+  _senderResponse: (response: {result: string}) => void
 ): void {
   if (sender.id !== chrome.runtime.id) return;
   if (request.message === "READ_SELECTION" && request.selection !== "")
