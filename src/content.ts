5fd91c29421c156f6a7711ae0987d277aa2e1f29
Lateef Alabi-Oki <mystilleef@gmail.com>
2021-09-29 00:57:35 -0400

    feat: reduce double click timeout

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-const DOUBLE_CLICK_TIMEOUT = 750;
+const DOUBLE_CLICK_TIMEOUT = 500;
 
 document.addEventListener("dblclick", _e => {
   window.setTimeout(sendSelectedTextMessage, DOUBLE_CLICK_TIMEOUT);
