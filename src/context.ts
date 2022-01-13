226ae04cc552a346d5b95a15755ae8effe7de185
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-13 03:26:24 -0500

    fix: type errors

--- src/context.ts
+++ src/context.ts
@@ -31,9 +31,8 @@ const VOICES_SUBMENU_ID_KEY = keyFromId(VOICES_MENU_ID);
 const READ_ALOUD_ROOT_MENU_ID = `ReadAloudMenu-${UNIQUE_STAMP}`;
 const RESET_DEFAULT_MENU_ID = `ReadAloudResetDefaultMenu-${UNIQUE_STAMP}`;
 
-type ContextType = string;
 
-const CONTEXTS = ["all"] as ContextType[];
+const CONTEXTS = ["all"] as chrome.contextMenus.ContextType[];
 const TOP_LEVEL_MENU_INFO = [
   {
     title: "Speed",
@@ -80,7 +79,7 @@ function createTopLevelMenus(menu: {
   id: string;
   title: string;
   parentId: string;
-  contexts: ContextType[];
+  contexts: chrome.contextMenus.ContextType[];
 }): void {
   chrome.contextMenus.create(menu, () => {
     if (chromeRuntimeError()) logChromeErrorMessage();
@@ -92,7 +91,7 @@ function createRadioMenuItems(menu: {
   id: string;
   title: string;
   parentId: string;
-  contexts: ContextType[];
+  contexts: chrome.contextMenus.ContextType[];
 }): void {
   switch (menu.id) {
     case SPEED_MENU_ID:
@@ -112,7 +111,7 @@ async function createSpeedRadioMenuItems(menu: {
   id: string;
   title: string;
   parentId: string;
-  contexts: ContextType[];
+  contexts: chrome.contextMenus.ContextType[];
 }): Promise<void> {
   const rate = await getRate();
   SPEED_OPTIONS.forEach(speed => {
@@ -135,7 +134,7 @@ async function createVoicesRadioMenuItems(menu: {
   id: string;
   title: string;
   parentId: string;
-  contexts: ContextType[];
+  contexts: chrome.contextMenus.ContextType[];
 }): Promise<void> {
   const voices = await getTtsVoices();
   const voiceName = await getVoiceName();
@@ -159,7 +158,7 @@ async function createPitchRadioMenuItems(menu: {
   id: string;
   title: string;
   parentId: string;
-  contexts: ContextType[];
+  contexts: chrome.contextMenus.ContextType[];
 }): Promise<void> {
   const pitchFromStore = await getPitch();
   PITCH_OPTIONS.forEach(pitch => {
