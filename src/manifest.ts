a604649cbc3337d8e594043b7dc20dadd3b87cda
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-13 16:38:17 -0500

    fix: manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,28 +2,28 @@
 import type { ManifestV2 } from "rollup-plugin-chrome-extension";
 
 const manifest: ManifestV2 = {
-  manifest_version: 2,
-  background: {
-    scripts: ["background.ts"],
-    persistence: false
+  "manifest_version": 2,
+  "background": {
+    "scripts": ["background.ts"],
+    "persistence": false
   },
-  content_scripts: [
+  "content_scripts": [
     {
-      js: ["content.ts"],
-      all_frames: true,
-      matches: ["<all_urls>"]
+      "js": ["content.ts"],
+      "all_frames": true,
+      "matches": ["<all_urls>"]
     }
   ],
-  browser_action: {
-    default_title: "Read aloud selected text",
-    default_icon: {
-      16: "images/default16.png",
-      32: "images/default32.png",
-      48: "images/default48.png",
-      128: "images/default128.png"
+  "browser_action": {
+    "default_title": "Read aloud selected text",
+    "default_icon": {
+      "16": "images/default16.png",
+      "32": "images/default32.png",
+      "48": "images/default48.png",
+      "128": "images/default128.png"
     }
   },
-  commands: {
+  "commands": {
     "read-aloud-selected-text": {
       "description": "Read aloud selected text",
       "suggested_key": { "default": "Ctrl+Space" }
@@ -35,7 +35,7 @@ const manifest: ManifestV2 = {
     "48": "images/default48.png",
     "128": "images/default128.png"
   },
-  web_accessible_resources: ["images/default.svg", "images/stop.svg"]
+  "web_accessible_resources": ["images/default.svg", "images/stop.svg"]
 };
 
 export default manifest;
