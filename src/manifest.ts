68183910cb100929a3d85dd49caf4774cadf3824
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-15 00:03:52 -0500

    refactor: inline constant

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,11 +2,6 @@ import { manifest as manifestv2 } from "./manifestv2";
 import { manifest as manifestv3 } from "./manifestv3";
 
 const verison = 2;
-const manifest = getManifest();
-
-function getManifest() {
-  if (verison === 2) return manifestv2;
-  return manifestv3;
-}
+const manifest = verison === 2 ? manifestv2 : manifestv3;
 
 export default manifest;
