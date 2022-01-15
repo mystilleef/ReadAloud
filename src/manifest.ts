7e910ad5047368fa427de5cd5779aefc648c7520
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-14 23:51:48 -0500

    feat: add quick way to change manifest versions

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,41 +1,12 @@
-/* eslint-disable camelcase */
-import type { ManifestV2 } from "rollup-plugin-chrome-extension";
+import { manifest as manifestv2 } from "./manifestv2";
+import { manifest as manifestv3 } from "./manifestv3";
 
-const manifest: ManifestV2 = {
-  "manifest_version": 2,
-  "background": {
-    "scripts": ["background.ts"],
-    "persistence": false
-  },
-  "content_scripts": [
-    {
-      "js": ["content.ts"],
-      "all_frames": true,
-      "matches": ["<all_urls>"]
-    }
-  ],
-  "browser_action": {
-    "default_title": "Read aloud selected text",
-    "default_icon": {
-      "16": "images/default16.png",
-      "32": "images/default32.png",
-      "48": "images/default48.png",
-      "128": "images/default128.png"
-    }
-  },
-  "commands": {
-    "read-aloud-selected-text": {
-      "description": "Read aloud selected text",
-      "suggested_key": { "default": "Ctrl+Space" }
-    }
-  },
-  "icons": {
-    "16": "images/default16.png",
-    "32": "images/default32.png",
-    "48": "images/default48.png",
-    "128": "images/default128.png"
-  },
-  "web_accessible_resources": ["images/default.svg", "images/stop.svg"]
-};
+const verison = 2;
+const manifest = getManifest();
+
+function getManifest() {
+  if (verison === 2) return manifestv2;
+  return manifestv3;
+}
 
 export default manifest;
