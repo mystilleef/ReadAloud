6a14968fcbb1dbdf681da3e2922b67a51e2ba515
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-15 14:08:53 -0500

    chore: migrate to vite

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,7 @@
 import { manifest as manifestv2 } from "./manifestv2";
 import { manifest as manifestv3 } from "./manifestv3";
 
-const verison = 2;
+const verison = 3;
 const manifest = verison < 3 ? manifestv2 : manifestv3;
 
 export default manifest;
