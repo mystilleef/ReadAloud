ccb34cbb3274d277bba7aecb3fc07ff96e85f952
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-14 23:31:06 -0500

    chore: update api based on manifest version

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,12 @@
-const EXTENSION_ID = chrome.runtime.id;
-const RATE = "rate";
-const PITCH = "pitch";
-const VOICENAME = "voiceName";
-export const chromeaction = chrome.browserAction;
+export const EXTENSION_ID = chrome.runtime.id;
+export const MANIFEST_VERSION = chrome.runtime.getManifest().version;
+export const RATE = "rate";
+export const PITCH = "pitch";
+export const VOICENAME = "voiceName";
+export const chromeaction = getChromeAction();
 
-export { RATE, PITCH, VOICENAME, EXTENSION_ID };
+function getChromeAction() {
+  if (MANIFEST_VERSION === "3") return chrome.action;
+  return chrome.browserAction;
+}
+
