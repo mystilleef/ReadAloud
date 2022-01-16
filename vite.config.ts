b840b7ef0ec3751a03fbbe558d362d74f538846a
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-16 02:36:54 -0500

    chore: add release build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { chromeExtension } from "rollup-plugin-chrome-extension";
 import { defineConfig } from "vite";
+import zip from "rollup-plugin-zip";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  root: "src",
-  build: { outDir: "../dist" },
-  plugins: [chromeExtension()]
+  plugins: [chromeExtension(), isProduction && zip({ dir: "releases" })]
 });
