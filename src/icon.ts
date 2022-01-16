efbab740905d907057cc10e8fc1d757f5c3a1dfd
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-16 03:26:40 -0500

    chore: add entry points for vite

--- src/icon.ts
+++ src/icon.ts
@@ -4,19 +4,18 @@ async function updateBrowserIcon(speaking: boolean): Promise<void> {
 }
 
 async function setDefaultIcon(): Promise<void> {
-  await setIcon("default");
+  setIcon("default");
   await setTooltip("Read aloud selected text");
 }
 
 async function setStopIcon(): Promise<void> {
-  await setIcon("stop");
+  setIcon("stop");
   await setTooltip("Stop reading");
 }
 
-async function setIcon(name: string): Promise<void> {
+function setIcon(name: string): void {
   const details = { path: `images/${name}.png` };
-  // eslint-disable-next-line @typescript-eslint/await-thenable
-  await chrome.action.setIcon(details);
+  chrome.action.setIcon(details);
 }
 
 async function setTooltip(tip: string): Promise<void> {
