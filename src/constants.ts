bd70afae7092c59e307666db3379a2b582d31b78
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-01-17 14:52:33 -0500

    refactor: rename

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const MANIFEST_VERSION = chrome.runtime.getManifest().version;
 export const RATE = "rate";
 export const PITCH = "pitch";
 export const VOICENAME = "voiceName";
+export const READ_ALOUD_COMMAND_STRING = "read_aloud_selected_text";
