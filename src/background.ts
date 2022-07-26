2421f7d4b92154ee7b7708bb5c2216bb8ce9cbe6
Lateef Alabi-Oki <mystilleef@gmail.com>
2022-07-26 12:12:55 -0400

    refactor: inline functions

--- src/background.ts
+++ src/background.ts
@@ -38,29 +38,25 @@ gotEndSpeakingStream
     if (sender.id === EXTENSION_ID) badgeCounter.decrement().catch(logError);
   });
 
-chrome.commands.onCommand.addListener(onChromeCommand);
-
-function onChromeCommand(command: string): void {
-  if (command === COMMAND) getSelectedTextFromContent();
-}
-
-chrome.action.onClicked.addListener(onAction);
-
-function onAction(_tab: chrome.tabs.Tab): void {
-  badgeCounter.reset().catch(logError);
-  const stopOrQuery = (speaking: boolean): void => {
-    speaking ? stop().catch(logError) : getSelectedTextFromContent();
-  };
-  isSpeaking().then(stopOrQuery).catch(logError);
-}
-
-function getSelectedTextFromContent(): void {
-  messageToContentScript(sendSelectedText, {}).catch(logError);
-}
+chrome.runtime.onInstalled.addListener(createContextMenu);
 
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === chrome.runtime.OnInstalledReason.INSTALL)
     storeDefaultOptions().catch(logError);
 });
 
-chrome.runtime.onInstalled.addListener(createContextMenu);
+chrome.commands.onCommand.addListener((command: string): void => {
+  if (command === COMMAND) selectTextFromContent();
+});
+
+chrome.action.onClicked.addListener((_tab: chrome.tabs.Tab): void => {
+  const stopOrQuery = (speaking: boolean): void => {
+    speaking ? stop().catch(logError) : selectTextFromContent();
+  };
+  isSpeaking().then(stopOrQuery).catch(logError);
+  badgeCounter.reset().catch(logError);
+});
+
+function selectTextFromContent(): void {
+  messageToContentScript(sendSelectedText, {}).catch(logError);
+}
