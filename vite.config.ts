76b1dc9864eef1b0055b2c28789483545013bb82
Lateef Alabi-Oki <mystilleef@gmail.com>
2023-02-10 01:34:28 -0500

    build: use eslint vite plugin for linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { crx } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import manifest from "./manifest.json";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import zipPack from "vite-plugin-zip-pack";
@@ -8,6 +9,7 @@ const PACKAGE_NAME = `readaloud-${manifest.version}.zip`;
 
 export default defineConfig({
   plugins: [
+    eslint({ cache: true, failOnWarning: true }),
     crx({ manifest }),
     viteStaticCopy({
       targets: [
